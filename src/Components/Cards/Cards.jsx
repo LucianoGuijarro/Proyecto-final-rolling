f0bb86269e912841f98825bcb7d2690efd2f9808
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-07-28 02:48:01 +0200

    modifico tamaño y estilos de cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -6,10 +6,8 @@ const Cards = ({ juego }) => {
     return (
         <>
             <div className={`${styles.card} col-sm-12 col-md-5 col-lg-2 d-flex flex-wrap justify-content-center mt-5  p-0`}>
-                {/* <h2 className={`${styles.titulo} text-center`}>{juego.nombre}</h2> */}
                 <img className={`${styles.img} img-fluid`} src={juego.portada} alt={juego.nombre} />
                 <div className='d-flex justify-content-around w-100'>
-                    {/* <p className='fw-bold fs-3'>{`€${juego.precio}`}</p> */}
                     <button className={`${styles.boton} w-100 opacity-75`}><TiShoppingCart size={35} color='white'/></button>
                 </div>
             </div>
