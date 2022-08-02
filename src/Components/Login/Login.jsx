ba97119e32273eb81508af657a11e2252667d912
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-02 19:17:44 -0300

    creo validaciones en front

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,71 +1,139 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 // import css
 import '../Login/Login.css';
 
 const Login = () => {
   const [newUserData, setNewUserData] = useState({
-    "userName": "",
-    "paswordUser":"",
-    "countryUser":""
+    "correoUser": "",
+    "nickNameUser": "",
+    "passwordUser": "",
+    "countryUser": ""
   })
-
-  const [errors, setErrors] = useState({})
+  const [confirmPasword, setConfirmPasword] = useState("")
+  const [errors, setErrors] = useState({
+    "correoUser": "*Este campo es obligatorio*",
+    "nickNameUser": "*Este campo es obligatorio*",
+    "passwordUser": "*Este campo es obligatorio*",
+    "confirmPassword": "Las contraseñas deben ser iguales",
+    "countryUser": "*Este campo es obligatorio*"
+  })
   const [loading, setLoading] = useState(false)
-  const [response, setResponse] = useState(null)
+  // const [response, setResponse] = useState(null)
 
-  const handleChange=( {target} )=>{
-    // console.log(target.value)
+  const handleChange = ({ target }) => {
     setNewUserData({
       ...newUserData,
       [target.name]: target.value
     })
+
   }
   // metodo post
-  const handleSubmit= async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    fetch('http://localhost:8080/users',{
-      method:'POST',
+    if (Object.keys(errors).length === 0) {
+      setLoading(true)
+    }
+    fetch('http://localhost:8080/users', {
+      method: 'POST',
       body: JSON.stringify(newUserData),
-      headers:{
-        'Content-type':'application/json; charset=UTF-8'
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
       }
     })
     .then(response => response.json())
-    .then((response)=>{
+    .then((response) => {
       console.log(response)
+      alert("El formulario se ah enviado")
+      setLoading(false)
     })
     .catch(error => console.log(error))
-
     e.target.reset()
   }
+
+  const handleBlur = ({ target }) => {
+    handleChange({ target })
+    setErrors(validateForm(newUserData))
+  }
+  const validateForm = () => {
+    let errores = {}
+    const emailValidation = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    const passwordValidation = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+
+
+    if (!newUserData.correoUser.trim()) {
+      errores.correoUser = "*Este Campo es obligatorio*"
+    } else
+      if (!emailValidation.test(newUserData.correoUser.trim())) {
+        errores.correoUser = "El email ingresado es incorrecto"
+      }
+
+    if (!newUserData.nickNameUser) {
+      errores.nickNameUser = "*Este campo es obligatorio*"
+    }
+
+    if (!newUserData.passwordUser.trim()) {
+      errores.passwordUser = "*Este Campo es obligatorio*"
+    } else if (passwordValidation.test(newUserData.passwordUser.trim())) {
+      errores.passwordUser = "Ingrese entre 8 y 16 caracteres, minusculas, mayusculas y digitos"
+    }
+
+    if (newUserData.passwordUser !== confirmPasword) {
+      errores.confirmPassword = "Las contraseñas no coinciden"
+    }
+    if (!newUserData.countryUser) {
+      errores.countryUser = "*Este campo es obligatorio*"
+    }
+    return errores
+  }
+
   return (
-    <div className="container col-4 form-styles p-4 align-items-center">
-      <h5 className="py-3 ">CREAR TU NUEVA CUENTA</h5>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="NewUserInputEmail1" name='userName' className="form-label">Correo electronico</label>
-          <input type="email" onChange={handleChange} className="form-control" id="NewUserInputEmail1" aria-describedby="emailHelp" />
-          {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="newUserInputPassword"  name='paswordUser' className="form-label">Contraseña</label>
-          <input type="password" onChange={handleChange} className="form-control" id="newUserInputPassword" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="newUserPasswordConfirm" className="form-label">Confirmar Contraseña</label>
-          <input type="password" onChange={handleChange} className="form-control" id="newUserPasswordConfirm" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="userCountry" name='countryUser' className="form-label">Pais de residencia</label>
-          <input type="text" onChange={handleChange} className="form-control" id="userCountry" />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="checkConditions" />
-          <label className="form-check-label" for="checkConditions">Confirmo que tengo 13 años o mas y acepto los terminos y condiciones</label>
-        </div>
-        <button type="submit" className="btn btn-crear-cuenta container-fluid">Crear Cuenta</button>
-      </form>
-    </div>
+    <>
+      {
+        loading ?
+          <div className="text-center pt-5">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+          :
+          <div className="container col-6 form-styles p-4 align-items-center">
+            <h5 className="py-3 ">CREAR TU NUEVA CUENTA</h5>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-2">
+                <label htmlFor="NewUserInputEmail1" className="form-label">Correo electronico</label>
+                <input type="email" onChange={handleChange} onBlur={handleBlur} name='correoUser' className="form-control" id="NewUserInputEmail1" aria-describedby="emailHelp" />
+                {errors.correoUser ? <p className='text-danger m-0'>{errors.correoUser}</p> : null}
+              </div>
+              <div className="mb-2">
+                <label htmlFor="nickNameUser" className="form-label">Nick Name</label>
+                <input type="text" onChange={handleChange} onBlur={handleBlur} name='nickNameUser' className="form-control" id="nickNameUser" aria-describedby="nickNameUser" />
+                {errors.nickNameUser ? <p className='text-danger m-0'>{errors.nickNameUser}</p> : null}
+              </div>
+              <div className="mb-2">
+                <label htmlFor="newUserInputPassword" className="form-label">Contraseña</label>
+                <input type="text" onChange={handleChange} onBlur={handleBlur} name='passwordUser' className="form-control" id="newUserInputPassword" />
+                {errors.passwordUser ? <p className='text-danger py-0'>{errors.passwordUser}</p> : null}
+              </div>
+              <div className="mb-2">
+                <label htmlFor="newUserPasswordConfirm" className="form-label">Confirmar Contraseña</label>
+                <input type="text" onChange={event => setConfirmPasword(event.target.value)} className="form-control" id="newUserPasswordConfirm" />
+                {errors.confirmPassword ? <p className='text-danger m-0'>{errors.confirmPassword}</p> : null}
+              </div>
+              <div className="mb-2">
+                <label htmlFor="userCountry" className="form-label">Pais de residencia</label>
+                <input type="text" onChange={handleChange} onBlur={handleBlur} name='countryUser' className="form-control" id="userCountry" />
+                {errors.countryUser ? <p className='text-danger m-0'>{errors.countryUser}</p> : null}
+              </div>
+              <div className="mb-2 form-check">
+                <input type="checkbox" className="form-check-input" id="checkConditions" />
+                <label className="form-check-label" for="checkConditions">Confirmo que tengo 13 años o mas y acepto los terminos y condiciones</label>
+              </div>
+              <button type="submit" className="btn btn-crear-cuenta container-fluid">Crear Cuenta</button>
+            </form>
+          </div>
+
+      }
+    </>
   )
 }
 
