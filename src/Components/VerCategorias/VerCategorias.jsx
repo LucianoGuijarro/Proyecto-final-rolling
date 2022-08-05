902aac40a3dd092278a6f54f97de180ca2f01cc3
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-06 00:54:43 +0200

    agregando seccion de agregar categorias en pag admin

--- src/Components/VerCategorias/VerCategorias.jsx
+++ src/Components/VerCategorias/VerCategorias.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import clientAxios from '../../Config/clientAxios';
+import { MdDeleteForever } from "react-icons/md";
+import styles from './VerCategorias.module.css';
 
 const VerCategorias = () => {
     const [categorias, setCategorias] = useState([]);
@@ -9,24 +11,28 @@ const VerCategorias = () => {
     }, [])
     return (
         <>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Acion</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        categorias.map((cat, i) => (
-                            <tr>
-                                <td>{cat.nombre}</td>
-                                <td>button</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+            <div className="container">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Acion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            categorias.map((cat, i) => (
+                                <tr>
+                                    <td>{cat.nombre}</td>
+                                    <td>
+                                        <button className={`${styles.botonEliminar}`}><MdDeleteForever size={25} color={'white'}/></button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
