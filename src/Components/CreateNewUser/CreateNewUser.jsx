92c7e33836c9aa3419346a09de220e246a2e10e4
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-05 23:30:34 -0300

    retoco estilos botones nav

--- src/Components/CreateNewUser/CreateNewUser.jsx
+++ src/Components/CreateNewUser/CreateNewUser.jsx
@@ -108,7 +108,7 @@ const CreateNewUser = () => {
             </div>
           </div>
           :
-          <div className={`container col-6 ${styles.form-styles} p-4 align-items-center`}>
+          <div className={`container col-5 ${styles.formStyles} p-2 align-items-center `}>
             <h5 className="py-3 ">CREAR TU NUEVA CUENTA</h5>
             <form onSubmit={handleSubmit}>
               <div className="mb-2">
