b3550b7f6da49bb79aa1d82392ced91b1e84e783
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-06 15:04:58 -0300

    trabajando en logica de buscador

--- src/Components/Buscador/Buscador.jsx
+++ src/Components/Buscador/Buscador.jsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import styles from '../Buscador/Buscador.module.css'
+import React, { useState } from "react";
+import clientAxios from "../../Config/clientAxios";
+import styles from "../Buscador/Buscador.module.css";
 
 const Buscador = () => {
-  const handleChange = () => {
+  const [search, setSearch] = useState(""); //guardo lo que busco en la tabla
+  const [juegos, setJuegos] = useState([]); //almaceno los juegos que encontre
 
-  }
+  const handleChange = async ({ target }) => {
+    setSearch(target.value)
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((response) => {
+        setJuegos(response)
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+    // await clientAxios.get('/juegos/verJuegos')
+  };
 
+  const handleSubmit = (e) => {
+    alert(`Buscando Resultados para ${search}`);
+    console.log(e);
+  };
 
   return (
     <div className={`mb-3 container mt-4 d-flex col-6 `}>
-      <input type="text" className="form-control col-2" placeholder="Buscar Juego" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-      <button type="button" className={`${styles.buton} px-2 ms-4`}>Buscar</button>
-    </div>
-  )
-}
+      <input
+        type="text"
+        className="form-control col-2"
+        placeholder="Buscar Juego"
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+        onChange={handleChange}
+      />
+      <button
+        type="button"
+        className={`${styles.buton} px-2 ms-4`}
+        onClick={handleSubmit}
+      >
+        Buscar
+      </button>
 
-export default Buscador
+      <ul className="dropdown-menu ">
+        <li>
+          <a className="dropdown-item" href="/">
+            Regular link
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="/" aria-current="true">
+            Active link
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="/">
+            Another link
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Buscador;
