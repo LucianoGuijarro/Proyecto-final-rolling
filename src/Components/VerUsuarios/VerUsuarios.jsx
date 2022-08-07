66713853b737fc09260d08aa83cb11058ab77e5d
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-07 22:10:51 +0200

    agregando funcion de eliminarUsuario

--- src/Components/VerUsuarios/VerUsuarios.jsx
+++ src/Components/VerUsuarios/VerUsuarios.jsx
@@ -3,13 +3,42 @@ import clientAxios from '../../Config/clientAxios';
 import { MdDeleteForever } from "react-icons/md";
 import { FiEdit } from "react-icons/fi";
 import styles from './VerUsuarios.module.css';
+import Swal from 'sweetalert2';
 
-const VerUsuarios = () => {
+const VerUsuarios = (id) => {
     const [usuarios, setUsuarios] = useState([])
     useEffect(() => {
         clientAxios.get('/users/verTodos')
             .then(response => setUsuarios(response.data))
     }, [])
+    const eliminarUsuario = (id) => {
+        if (window.confirm('Estas seguro de eliminar este usuario')) {
+            clientAxios.delete(`/users/eliminarUsuario/${id}`)
+                .then(response => {
+                    if (response.status !== 200) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: 'Ha ocurrido un error y no se pudo eliminar el usuario',
+                        })
+                    } else {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Usuario eliminado',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                }).catch(error => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Usuario eliminado',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                })
+        }
+    }
     return (
         <>
             <div className="container">
@@ -29,7 +58,7 @@ const VerUsuarios = () => {
                                     <td className='text-light'>{user.nickNameUser}</td>
                                     <td>
                                         <button className={`${styles.boton}`}><FiEdit size={25} color={'white'} /></button>
-                                        <button className={`${styles.boton}`}><MdDeleteForever size={25} color={'white'} /></button>
+                                        <button className={`${styles.boton}`} onClick={() => eliminarUsuario(user._id)}><MdDeleteForever size={25} color={'white'} /></button>
                                     </td>
                                 </tr>
                             ))
@@ -41,4 +70,4 @@ const VerUsuarios = () => {
     )
 }
 
-export default VerUsuarios
+export default VerUsuarios;
