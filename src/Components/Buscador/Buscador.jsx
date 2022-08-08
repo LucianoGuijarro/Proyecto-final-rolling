097ebf3206c0025a65c56f45f243635bd1c03616
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-07 21:14:06 -0300

    muevo boton admin a user

--- src/Components/Buscador/Buscador.jsx
+++ src/Components/Buscador/Buscador.jsx
@@ -9,7 +9,7 @@ const Buscador = () => {
   const [resultsSerch, setResultsSerch] = useState([]); //almacenar los juegos filtrados
 
   const handleChange = async ({ target }) => {
-    setSearch(target.value)
+    setSearch(target.value);
     clientAxios.get("/juegos/verJuegos").then((response) => {
       filterNames(search);
       setJuegos(response.data);
@@ -45,7 +45,7 @@ const Buscador = () => {
 
   const captureInfo = (e) => {};
   return (
-    <div className={`container dropdown`}>
+    <div className={`container dropdown d-flex align-items-center`}>
       <input
         type="text"
         className="form-control col-2 dropdown-toggle"
@@ -57,6 +57,16 @@ const Buscador = () => {
         aria-describedby="basic-addon2"
         onChange={handleChange}
       />
+      {/* <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="100"
+        fill="currentColor"
+        class="bi bi-search"
+        viewBox="100 100 100 100"
+      >
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg> */}
       <ul className="dropdown-menu container">
         <li>
           {resultsSerch.length <= 1 ? (
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { } from 'react';
-import { Link } from 'react-router-dom';
-import Buscador from '../Buscador/Buscador'
-import styles from './Navbar.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Buscador from "../Buscador/Buscador";
+import styles from "./Navbar.module.css";
 
-function Navbar({rol}) {
+function Navbar({ rol }) {
   // const [nickNameStorage, setNickNameStorage] = useState()
   // const any = localStorage.getItem('nickName')
   // setNickNameStorage(localStorage.getItem('nickName'))
@@ -17,73 +17,172 @@ function Navbar({rol}) {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <Link className="navbar-brand" to=''> <img src="./logo2.png" className={`${styles.img} d-block w-100`} alt="Logo" /></Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="">
+          {" "}
+          <img
+            src="./logo2.png"
+            className={`${styles.img} d-block w-100`}
+            alt="Logo"
+          />
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        <div
+          className="collapse navbar-collapse justify-content-between"
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav  mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className={`${styles.text} nav-link active text-white`} aria-current="page" to='/'>INICIO</Link>
+              <Link
+                className={`${styles.text} nav-link active text-white`}
+                aria-current="page"
+                to="/"
+              >
+                INICIO
+              </Link>
             </li>
             <li className="nav-item dropdown">
-              <Link className={`${styles.text} nav-link dropdown-toggle text`} to='/' id="navbarDropdown" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
+              <Link
+                className={`${styles.text} nav-link dropdown-toggle text`}
+                to="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 CATEGORIAS
               </Link>
-              <ul className={`${styles.bgColorDropdown} dropdown-menu `} aria-labelledby="navbarDropdown">
-                <li><Link className={`${styles.text} dropdown-item`} to='/categoria/accion'>Accion</Link></li>
-                <li><Link className={`${styles.text} dropdown-item`} to='/categoria/aventura'>Aventura</Link></li>
-                <li><Link className={`${styles.text} dropdown-item`} to='/categoria/deportes'>Deportes</Link></li>
+              <ul
+                className={`${styles.bgColorDropdown} dropdown-menu `}
+                aria-labelledby="navbarDropdown"
+              >
+                <li>
+                  <Link
+                    className={`${styles.text} dropdown-item`}
+                    to="/categoria/accion"
+                  >
+                    Accion
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={`${styles.text} dropdown-item`}
+                    to="/categoria/aventura"
+                  >
+                    Aventura
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={`${styles.text} dropdown-item`}
+                    to="/categoria/deportes"
+                  >
+                    Deportes
+                  </Link>
+                </li>
               </ul>
-            </li>          
-            {
-              rol === 'admin' ? <li><Link to={'/admin'} className={`${styles.text} nav-link active text-white`}>Admin</Link></li> : false
-            }
+            </li>
           </ul>
           <ul className="navbar-nav d-flex justify-content-end align-items-center col-6">
             <Buscador />
-            {
-              localStorage.getItem("nickName") != null
-                // nickNameStorage == null
-                ?
-                <>
-                  <li className="nav-item">
-                    <div className="btn-group" role="group">
-                      <button id="btnGroupDrop1" type="button" className="btn dropdown-toggle text-white" data-bs-toggle="dropdown" aria-expanded="false">
-                        User: {localStorage.getItem("nickName")}
-                      </button>
-                      <ul className={`dropdown-menu ${styles.bgColorDropdown}`} aria-labelledby="btnGroupDrop1">
-                        {/* <li><button className={`{styles.text} dropdown-item text-white`} href="/">Perfil</button></li> */}
-                        <li><button className={`{styles.text} dropdown-item text-white`} onClick={() => localStorage.clear()} href="/">Cerrar sesion</button></li>
-                        {/* <li><a className={`{styles.text} dropdown-item text-white`} href="/">Dropdown link</a></li> */}
-                      </ul>
-                    </div>
-                    {/* <button type="button" className={`btn px-1 py-1  me-2`}>
+            {localStorage.getItem("nickName") != null ? (
+              // nickNameStorage == null
+              <>
+                <li className="nav-item">
+                  <div className="btn-group" role="group">
+                    <button
+                      id="btnGroupDrop1"
+                      type="button"
+                      className="btn dropdown-toggle text-white"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      User: {localStorage.getItem("nickName")}
+                    </button>
+                    <ul
+                      className={`dropdown-menu ${styles.bgColorDropdown}`}
+                      aria-labelledby="btnGroupDrop1"
+                    >
+                      {/* <li><button className={`{styles.text} dropdown-item text-white`} href="/">Perfil</button></li> */}
+                      <li>
+                        <button
+                          className={`{styles.text} dropdown-item text-white`}
+                          onClick={() => localStorage.clear()}
+                          href="/"
+                        >
+                          Cerrar sesion
+                        </button>
+                      </li>
+                      <li>
+                        {" "}
+                        {rol === "admin" ? (
+                          <li>
+                            <Link
+                              to={"/admin"}
+                              className={`${styles.text} dropdown-item text-white`}
+                            >
+                              Admin
+                            </Link>
+                          </li>
+                        ) : (
+                          false
+                        )}
+                      </li>
+                      {/* <li><a className={`{styles.text} dropdown-item text-white`} href="/">Dropdown link</a></li> */}
+                    </ul>
+                  </div>
+                  {/* <button type="button" className={`btn px-1 py-1  me-2`}>
                     
                       <Link className={`${styles.text} nav-link active text-white`} aria-current="page" to='/login'>User: {localStorage.getItem("nickName")}</Link>
                     </button> */}
-                  </li>
-                </>
-                :
-                <>
-                  <li className="nav-item">
-                    <button type="button" className={`btn px-1 py-1 ${styles.butonIniciar} me-2`}>
-                      <Link className={`${styles.text} nav-link active text-white`} aria-current="page" to='/login'>Iniciar Sesion</Link>
-                    </button>
-                  </li>
-                  <li className="nav-item">
-                    <button type="button" className={`btn px-1 py-1  ${styles.buton}`}>
-                      <Link className={`${styles.text} nav-link active text-white`} aria-current="page" to='/createNewUser'>Registrate</Link>
-                    </button>
-                  </li>
-                </>
-            }
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <button
+                    type="button"
+                    className={`btn px-1 py-1 ${styles.butonIniciar} me-2`}
+                  >
+                    <Link
+                      className={`${styles.text} nav-link active text-white`}
+                      aria-current="page"
+                      to="/login"
+                    >
+                      Iniciar Sesion
+                    </Link>
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button
+                    type="button"
+                    className={`btn px-1 py-1  ${styles.buton}`}
+                  >
+                    <Link
+                      className={`${styles.text} nav-link active text-white`}
+                      aria-current="page"
+                      to="/createNewUser"
+                    >
+                      Registrate
+                    </Link>
+                  </button>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
