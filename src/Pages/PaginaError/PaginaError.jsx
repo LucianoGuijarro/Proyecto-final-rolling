319fa58f5b41c3620c740f2fb82e4779e0b609c0
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-09 14:33:36 +0200

    arreglando deploy

--- src/Pages/PaginaError/PaginaError.jsx
+++ src/Pages/PaginaError/PaginaError.jsx
@@ -5,7 +5,7 @@ import styles from './PaginaError.module.css';
 function PaginaError() {
     return (
         <div className= {`${styles.container} container`}>
-            <img className= {`${styles.img}`} src="./error404.png" alt="error404" />
+            <img className= {`${styles.img}`} src="./error404net.jpg" alt="error404" />
             <h1 className='text-light'>La pagina que ha solicitado no se encuentra disponible</h1>
             <button type="button" class={`${styles.boton} btn btn-light`}>
                 <Link to='/'> Ir a pagina de inicio </Link>
