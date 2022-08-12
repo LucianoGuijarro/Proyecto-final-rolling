84e80fb3450d634449e2983928f33a58e9ee299e
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-12 16:48:19 -0300

    corrijo identacion

--- src/Components/VerCategorias/VerCategorias.jsx
+++ src/Components/VerCategorias/VerCategorias.jsx
@@ -1,70 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import clientAxios from '../../Config/clientAxios';
+import React, { useEffect, useState } from "react";
+import clientAxios from "../../Config/clientAxios";
 import { MdDeleteForever } from "react-icons/md";
-import styles from './VerCategorias.module.css';
-import Swal from 'sweetalert2';
+import styles from "./VerCategorias.module.css";
+import Swal from "sweetalert2";
 
 const VerCategorias = () => {
-    const [categorias, setCategorias] = useState([]);
-    const [flag, setFlag] = useState(true)
-    useEffect(() => {
-        clientAxios.get('/categorias/verCategorias')
-            .then(response => setCategorias(response.data))
-    }, [flag])
-    const eliminarCategoria = (id) => {
-        if (window.confirm('Seguro quieres eliminar esta categoria?')) {
-            clientAxios.delete(`/categorias/eliminarCategoria/${id}`)
-                .then(response => {
-                    if (response.status !== 200) {
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Error',
-                            text: 'Ha ocurrido un error y no se pudo eliminar esta categoria',
-                        })
-                    } else {
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Categoria eliminada',
-                            showConfirmButton: false,
-                            timer: 1500
-                        })
-                        setFlag(!flag)
-                    }
-                }).catch(error => {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Ha ocurrido un error y no se pudo esta categoria',
-                      })
-                })
-        }
+  const [categorias, setCategorias] = useState([]);
+  const [flag, setFlag] = useState(true);
+  useEffect(() => {
+    clientAxios
+      .get("/categorias/verCategorias")
+      .then((response) => setCategorias(response.data));
+  }, [flag]);
+  const eliminarCategoria = (id) => {
+    if (window.confirm("Seguro quieres eliminar esta categoria?")) {
+      clientAxios
+        .delete(`/categorias/eliminarCategoria/${id}`)
+        .then((response) => {
+          if (response.status !== 200) {
+            Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Ha ocurrido un error y no se pudo eliminar esta categoria",
+            });
+          } else {
+            Swal.fire({
+              icon: "success",
+              title: "Categoria eliminada",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            setFlag(!flag);
+          }
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Ha ocurrido un error y no se pudo esta categoria",
+          });
+        });
     }
-    return (
-        <>
-            <div className="container">
-                <table className="table">
-                    <thead>
-                        <tr className='text-light'>
-                            <th scope="col">Categoria</th>
-                            <th scope="col">Accion</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            categorias.map((cat, i) => (
-                                <tr key={i}>
-                                    <td className='text-light' >{cat.nombre}</td>
-                                    <td>
-                                        <button className={`${styles.boton}`} onClick={() => eliminarCategoria(cat._id)}><MdDeleteForever size={25} color={'white'} /></button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr className="text-light">
+              <th scope="col">Categoria</th>
+              <th scope="col">Accion</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categorias.map((cat, i) => (
+              <tr key={i}>
+                <td className="text-light">{cat.nombre}</td>
+                <td>
+                  <button
+                    className={`${styles.boton}`}
+                    onClick={() => eliminarCategoria(cat._id)}
+                  >
+                    <MdDeleteForever size={25} color={"white"} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default VerCategorias
+export default VerCategorias;
