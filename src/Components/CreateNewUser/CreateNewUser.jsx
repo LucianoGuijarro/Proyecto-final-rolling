8b6b880a3e832bf05568bc2f39ca10188f3e080f
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-12 16:45:21 -0300

    agrego loader a registerPage

--- src/Components/CreateNewUser/CreateNewUser.jsx
+++ src/Components/CreateNewUser/CreateNewUser.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import styles from '../CreateNewUser/CreateNewUser.module.css';
-import Swal from 'sweetalert2';
-import clientAxios from '../../Config/clientAxios';
+import styles from "../CreateNewUser/CreateNewUser.module.css";
+import Swal from "sweetalert2";
+import clientAxios from "../../Config/clientAxios";
 
 const CreateNewUser = () => {
   const navigate = useNavigate();
@@ -10,148 +10,233 @@ const CreateNewUser = () => {
     correoUser: "",
     nickNameUser: "",
     passwordUser: "",
-    countryUser: ""
-  })
-  const [confirmPasword, setConfirmPasword] = useState("")
+    countryUser: "",
+  });
+  const [confirmPasword, setConfirmPasword] = useState("");
   const [errors, setErrors] = useState({
     correoUser: "*Este campo es obligatorio*",
     nickNameUser: "*Este campo es obligatorio*",
     passwordUser: "*Este campo es obligatorio*",
     confirmPassword: "Las contraseñas deben ser iguales",
-    countryUser: "*Este campo es obligatorio*"
-  })
-  const [loading, setLoading] = useState(false)
-  const [userCreated, setUserCreated] = useState(false)
-  const [disable, setDisable] = useState(true)
+    countryUser: "*Este campo es obligatorio*",
+  });
+  const [loading, setLoading] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [disable, setDisable] = useState(true);
 
   const handleChange = ({ target }) => {
     setNewUserData({
       ...newUserData,
-      [target.name]: target.value
-    })
-  }
+      [target.name]: target.value,
+    });
+  };
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (Object.keys(errors).length === 0) {
-      setLoading(true)
-      clientAxios.post('/users/agregarUsuario', newUserData)
-        .then(response => {
+      setLoading(true);
+      clientAxios
+        .post("/users/agregarUsuario", newUserData)
+        .then((response) => {
           if (response.status === 201) {
             Swal.fire({
-              icon: 'success',
-              title: 'Usuario creado con exito',
+              icon: "success",
+              title: "Usuario creado con exito",
               showConfirmButton: false,
-              timer: 1500
-            })
-            setUserCreated(true)
-            setLoading(false)
-            navigate('/login')
+              timer: 1500,
+            });
+            setUserCreated(true);
+            setLoading(false);
+            navigate("/login");
           } else {
             Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Este mail ya esta en uso',
-            })
-            navigate('./CreateNewUser.jsx')
+              icon: "error",
+              title: "Oops...",
+              text: "Este mail ya esta en uso",
+            });
+            navigate("./CreateNewUser.jsx");
+            setLoading(false);
           }
-        }).catch(error => {
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Ha ocurrido un error y el usuario no se creo correcamente',
-          })
         })
-        e.target.reset()
-    } else alert("Hay campos incompletos")
-  }
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Ha ocurrido un error y el usuario no se creo correcamente",
+          });
+        });
+      e.target.reset();
+    } else alert("Hay campos incompletos");
+  };
 
   const handleBlur = ({ target }) => {
-    handleChange({ target })
-    setErrors(validateForm(newUserData))
-  }
+    handleChange({ target });
+    setErrors(validateForm(newUserData));
+  };
 
   const validateForm = () => {
-    let errores = {}
+    let errores = {};
     const emailValidation = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-    const passwordValidation = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+    const passwordValidation = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
 
     if (!newUserData.correoUser.trim()) {
-      errores.correoUser = "*Este Campo es obligatorio*"
-    } else
-      if (!emailValidation.test(newUserData.correoUser.trim())) {
-        errores.correoUser = "El email ingresado es incorrecto"
-      }
+      errores.correoUser = "*Este Campo es obligatorio*";
+    } else if (!emailValidation.test(newUserData.correoUser.trim())) {
+      errores.correoUser = "El email ingresado es incorrecto";
+    }
 
     if (!newUserData.nickNameUser) {
-      errores.nickNameUser = "*Este campo es obligatorio*"
+      errores.nickNameUser = "*Este campo es obligatorio*";
     }
 
     if (!newUserData.passwordUser.trim()) {
-      errores.passwordUser = "*Este Campo es obligatorio*"
+      errores.passwordUser = "*Este Campo es obligatorio*";
     } else if (!passwordValidation.test(newUserData.passwordUser.trim())) {
-      errores.passwordUser = "Ingrese entre 8 y 16 caracteres, minusculas, mayusculas y digitos"
+      errores.passwordUser =
+        "Ingrese entre 8 y 16 caracteres, minusculas, mayusculas y digitos";
     }
 
     if (newUserData.passwordUser !== confirmPasword) {
-      errores.confirmPassword = "Las contraseñas no coinciden"
+      errores.confirmPassword = "Las contraseñas no coinciden";
       console.log("valido confirm");
     }
     if (!newUserData.countryUser) {
-      errores.countryUser = "*Este campo es obligatorio*"
+      errores.countryUser = "*Este campo es obligatorio*";
     }
-    return errores
-  }
+    return errores;
+  };
+
+  // <div className="d-flex justify-content-center vh-100">
+  //   <div className="spinner-border" role="status">
+  //     <span className="visually-hidden">Loading...</span>
+  //   </div>
+  // </div>;
 
   return (
     <>
-      {
-        loading ?
-          <div className="text-center pt-5">
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
+      {loading ? (
+        <div className="d-flex justify-content-center vh-100">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`container col-sm-10 col-md-5 col-6 ${styles.formStyles} p-2 align-items-center`}
+        >
+          <h5 className="py-3 ">CREAR TU NUEVA CUENTA</h5>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-2">
+              <label htmlFor="NewUserInputEmail1" className="form-label">
+                Correo electronico
+              </label>
+              <input
+                type="email"
+                maxLength={40}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                name="correoUser"
+                className="form-control"
+                id="NewUserInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {errors.correoUser ? (
+                <p className="text-danger m-0">{errors.correoUser}</p>
+              ) : null}
             </div>
-          </div>
-          :
-          <div className={`container col-sm-10 col-md-5 col-6 ${styles.formStyles} p-2 align-items-center`}>
-            <h5 className="py-3 ">CREAR TU NUEVA CUENTA</h5>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-2">
-                <label htmlFor="NewUserInputEmail1" className="form-label">Correo electronico</label>
-                <input type="email" maxLength={40} onChange={handleChange} onBlur={handleBlur} name='correoUser' className="form-control" id="NewUserInputEmail1" aria-describedby="emailHelp" />
-                {errors.correoUser ? <p className='text-danger m-0'>{errors.correoUser}</p> : null}
-              </div>
-              <div className="mb-2">
-                <label htmlFor="nickNameUser" className="form-label">Nick Name</label>
-                <input type="text" maxLength={20} onChange={handleChange} onBlur={handleBlur} name='nickNameUser' className="form-control" id="nickNameUser" aria-describedby="nickNameUser" />
-                {errors.nickNameUser ? <p className='text-danger m-0'>{errors.nickNameUser}</p> : null}
-              </div>
-              <div className="mb-2">
-                <label htmlFor="newUserInputPassword" className="form-label">Contraseña</label>
-                <input type="password" maxLength={40} onChange={handleChange} onBlur={handleBlur} name='passwordUser' className="form-control" id="newUserInputPassword" />
-                {errors.passwordUser ? <p className='text-danger py-0'>{errors.passwordUser}</p> : null}
-              </div>
-              <div className="mb-2">
-                <label htmlFor="newUserPasswordConfirm" className="form-label">Confirmar Contraseña</label>
-                <input type="password" maxLength={40} onChange={event => setConfirmPasword(event.target.value)} onBlur={handleBlur} className="form-control" id="newUserPasswordConfirm" />
-                {errors.confirmPassword ? <p className='text-danger m-0'>{errors.confirmPassword}</p> : null}
-              </div>
-              <div className="mb-2">
-                <label htmlFor="userCountry" className="form-label">Pais de residencia</label>
-                <input type="text" maxLength={20} onChange={handleChange} onBlur={handleBlur} name='countryUser' className="form-control" id="userCountry" />
-                {errors.countryUser ? <p className='text-danger m-0'>{errors.countryUser}</p> : null}
-              </div>
-              <div className="mb-2 form-check">
-                <input type="checkbox" className="form-check-input" id="checkConditions" onClick={()=>setDisable(!disable)}/>
-                <label className="form-check-label" htmlFor="checkConditions">Confirmo que tengo 13 años o mas y acepto los terminos y condiciones</label>
-              </div>
-              {userCreated ? <Navigate to="/Login" /> : null}
-              <button type="submit" className={`${styles.btnCreateAcount} btn  container-fluid`} disabled={disable}>Crear Cuenta</button>
-            </form>
-          </div>
-      }
+            <div className="mb-2">
+              <label htmlFor="nickNameUser" className="form-label">
+                Nick Name
+              </label>
+              <input
+                type="text"
+                maxLength={20}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                name="nickNameUser"
+                className="form-control"
+                id="nickNameUser"
+                aria-describedby="nickNameUser"
+              />
+              {errors.nickNameUser ? (
+                <p className="text-danger m-0">{errors.nickNameUser}</p>
+              ) : null}
+            </div>
+            <div className="mb-2">
+              <label htmlFor="newUserInputPassword" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                maxLength={40}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                name="passwordUser"
+                className="form-control"
+                id="newUserInputPassword"
+              />
+              {errors.passwordUser ? (
+                <p className="text-danger py-0">{errors.passwordUser}</p>
+              ) : null}
+            </div>
+            <div className="mb-2">
+              <label htmlFor="newUserPasswordConfirm" className="form-label">
+                Confirmar Contraseña
+              </label>
+              <input
+                type="password"
+                maxLength={40}
+                onChange={(event) => setConfirmPasword(event.target.value)}
+                onBlur={handleBlur}
+                className="form-control"
+                id="newUserPasswordConfirm"
+              />
+              {errors.confirmPassword ? (
+                <p className="text-danger m-0">{errors.confirmPassword}</p>
+              ) : null}
+            </div>
+            <div className="mb-2">
+              <label htmlFor="userCountry" className="form-label">
+                Pais de residencia
+              </label>
+              <input
+                type="text"
+                maxLength={20}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                name="countryUser"
+                className="form-control"
+                id="userCountry"
+              />
+              {errors.countryUser ? (
+                <p className="text-danger m-0">{errors.countryUser}</p>
+              ) : null}
+            </div>
+            <div className="mb-2 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="checkConditions"
+                onClick={() => setDisable(!disable)}
+              />
+              <label className="form-check-label" htmlFor="checkConditions">
+                Confirmo que tengo 13 años o mas y acepto los terminos y
+                condiciones
+              </label>
+            </div>
+            {userCreated ? <Navigate to="/Login" /> : null}
+            <button
+              type="submit"
+              className={`${styles.btnCreateAcount} btn  container-fluid`}
+              disabled={disable}
+            >
+              Crear Cuenta
+            </button>
+          </form>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default CreateNewUser;
