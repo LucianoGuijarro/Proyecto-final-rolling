e94d958a4019a86794ff79a30ad7a4ab06d1cf01
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-17 01:29:50 +0200

    arreglo responsive de formulario agregar juego y cambio input por select en categorias en el formulario de agregar juego

--- src/Components/AgregarJuego/AgregarJuego.jsx
+++ src/Components/AgregarJuego/AgregarJuego.jsx
@@ -9,6 +9,7 @@ const AgregarJuego = () => {
     });
     const handleForm = (e) => {
         e.preventDefault();
+        console.log(juegoNuevo)
         clientAxios.post('/juegos/agregarJuego', {
             nombre: juegoNuevo.nombre,
             fechaLanzamiento: juegoNuevo.fechaLanzamiento,
@@ -26,13 +27,13 @@ const AgregarJuego = () => {
                     title: 'Juego agregado correctamente',
                     showConfirmButton: false,
                     timer: 1500
-                  })
+                })
             } else {
                 Swal.fire({
                     icon: 'error',
                     title: 'Error',
                     text: 'Ha ocurrido un error y no se pudo agregar el juego',
-                  })
+                })
             }
             e.target.reset()
         }).catch(error => {
@@ -40,15 +41,15 @@ const AgregarJuego = () => {
                 icon: 'error',
                 title: 'Error',
                 text: 'Ha ocurrido un error y no se pudo agregar el juego',
-              })
+            })
         })
     }
     const handleChange = (e) => {
-    const { name, value } = e.target;
-    setJuegoNuevo((prevState) => ({
-        ...prevState,
-        [name]: value
-    }))
+        const { name, value } = e.target;
+        setJuegoNuevo((prevState) => ({
+            ...prevState,
+            [name]: value
+        }))
     }
     return (
         <>
@@ -58,35 +59,44 @@ const AgregarJuego = () => {
                     <div className="row flex-column">
                         <div className='my-4'>
                             <label className='text-light' htmlFor="nombre">Nombre</label>
-                            <input clas maxLength={40} className={`${styles.inputJuego} ms-3`} type="text" id='nombre' name='nombre' onChange={handleChange} required />
+                            <input clas maxLength={40} className={`${styles.inputJuego} form-control ms-3`} type="text" id='nombre' name='nombre' onChange={handleChange} required />
                         </div>
                         <div className='my-4'>
                             <label className='text-light' htmlFor="fechaLanzamiento">Fecha de Lanzamiento</label>
-                            <input className={`${styles.inputJuego} ms-3`} type="date" max={'2024-01-01'} min={'1990-01-01'} id='fechaLanzamiento' name='fechaLanzamiento' onChange={handleChange} required />
+                            <input className={`${styles.inputJuego} form-control ms-3`} type="date" max={'2024-01-01'} min={'1990-01-01'} id='fechaLanzamiento' name='fechaLanzamiento' onChange={handleChange} required />
                         </div>
-                        <div className='my-4'>
+                        {/* <div className='my-4'>
                             <label className='text-light' htmlFor="categoria">Categoria</label>
                             <input maxLength={15} className={` ${styles.inputJuego} ms-3`} type="text" id='categoria' name='categoria' onChange={handleChange} required />
+                        </div> */}
+                        <div className='my-4'>
+                            <label className='text-light form-label' htmlFor="categoria">Categoria</label>
+                            <select class={`${styles.inputJuego} form-control ms-3`} id='categoria' name='categoria' onChange={handleChange}>
+                                <option selected>Seleccione una categoria</option>
+                                <option value="accion">Accion</option>
+                                <option value="aventura">Aventura</option>
+                                <option value="deportes">Deportes</option>
+                            </select>
                         </div>
                         <div className='my-4'>
-                            <label className='text-light' htmlFor="precio">Precio</label>
-                            <input maxLength={5} className={` ${styles.inputJuego} ms-3`} min={1} type="number" id='precio' name='precio' onChange={handleChange} required />
+                            <label className='text-light form-label' htmlFor="precio">Precio</label>
+                            <input maxLength={5} className={` ${styles.inputJuego} form-control ms-3`} min={1} type="number" id='precio' name='precio' onChange={handleChange} required />
                         </div>
                         <div className='my-4'>
-                            <label className='text-light' htmlFor="portada">Portada</label>
-                            <input className={`${styles.inputJuego} ms-3`} type="url" id='portada' name='portada' onChange={handleChange} required />
+                            <label className='text-light form-label' htmlFor="portada">Portada</label>
+                            <input className={`${styles.inputJuego} form-control ms-3`} type="url" id='portada' name='portada' onChange={handleChange} required />
                         </div>
                         <div className='my-4'>
-                            <label className='text-light' htmlFor="slider">Slider</label>
-                            <input className={`${styles.inputJuego} ms-3`} type="url" id='slider' name='slider' onChange={handleChange} required />
+                            <label className='text-light form-label' htmlFor="slider">Slider</label>
+                            <input className={`${styles.inputJuego} form-control ms-3`} type="url" id='slider' name='slider' onChange={handleChange} required />
                         </div>
                         <div className='my-4'>
-                            <label className='text-light' htmlFor="trailer">Trailer</label>
-                            <input className={` ${styles.inputJuego} ms-3`} type="url" id='trailer' onChange={handleChange} name='trailer' />
+                            <label className='text-light form-label' htmlFor="trailer">Trailer</label>
+                            <input className={` ${styles.inputJuego} form-control ms-3`} type="url" id='trailer' onChange={handleChange} name='trailer' />
                         </div>
-                        <div className='my-4 d-flex align-items-center'>
-                            <label className='text-light' htmlFor="sinopsis">Sinopsis</label>
-                            <textarea cols={50} maxLength={500} rows={6} className={`ms-3`} type="text" id='sinopsis' onChange={handleChange} name='sinopsis' required />
+                        <div className='my-4'>
+                            <label className='text-light form-label' htmlFor="sinopsis">Sinopsis</label>
+                            <textarea cols={40} maxLength={500} rows={6} className={`ms-3 form-control`} type="text" id='sinopsis' onChange={handleChange} name='sinopsis' required />
                         </div>
                         <button className='btn btn-success' type='submit'>Agregar Juego</button>
                     </div>
